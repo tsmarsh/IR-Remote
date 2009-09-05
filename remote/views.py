8204315238f95be91a6a5d126d9a4eef6eb2af5c
Tom Marsh <ts.marsh@gmail.com>
2009-09-05 09:35:32 -0400

    [TomM] Functions don't return the full page any more.
    Signed-off-by: Tom Marsh <ts.marsh@gmail.com>

--- remote/views.py
+++ remote/views.py
@@ -6,28 +6,28 @@ def index(request):
 
 def frontrow(request):
 	app(u'System Events').key_code(53, using=k.command_down)
-	return render_to_response('remote.xhtml', mimetype='application/xhtml+xml')
+	return render_to_response('200.html')
 	
 def play(request):
 	app(u'System Events').key_code(36)
-	return render_to_response('remote.xhtml', mimetype='application/xhtml+xml')
+	return render_to_response('200.html')
 
 def menu(request):
 	app(u'System Events').key_code(53)
-	return render_to_response('remote.xhtml', mimetype='application/xhtml+xml')
+	return render_to_response('200.html')
 
 def up(request):
 	app(u'System Events').key_code(126)
-	return render_to_response('remote.xhtml', mimetype='application/xhtml+xml')
+	return render_to_response('200.html')
 			
 def down(request):
 	app(u'System Events').key_code(125)
-	return render_to_response('remote.xhtml', mimetype='application/xhtml+xml')
+	return render_to_response('200.html')
 	
 def skip_back(request):
 	app(u'System Events').key_code(123)
-	return render_to_response('remote.xhtml', mimetype='application/xhtml+xml')
+	return render_to_response('200.html')
 
 def skip_forward(request):
 	app(u'System Events').key_code(124)
-	return render_to_response('remote.xhtml', mimetype='application/xhtml+xml')
+	return render_to_response('200.html')
