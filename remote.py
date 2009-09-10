468977cbd4e9e961f3517689228ceb53c02d1c51
Tom Marsh <ts.marsh@gmail.com>
2009-09-10 18:41:31 -0400

    [TomM] Modified to use tornado

--- remote.py
+++ remote.py
@@ -1,45 +1,40 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
 import re
 import os
 import pybonjour
 
-class RemoteHandler(BaseHTTPRequestHandler):
-	
-	def render_to_response(self, file, mimetype='text/html'):
-		self.send_response(200)
-		self.send_header('Content-type', mimetype)
-		self.end_headers()
-		self.wfile.write(open(file, 'r').read())
-	
-	def runscript(self, name):
-		os.system("osascript applescripts/" + name)
-			
-	def do_GET(self):
-		if re.match(r'^/$', self.path):
-			return self.render_to_response('remote.xhtml', mimetype='application/xhtml+xml')
-		else:
-			match = re.match(r'^/(.*)', self.path)
-			if match:
-				self.runscript(match.group(1) + ".scpt")
-				return self.render_to_response('200.html')
+port = 8888
+
+class RemoteHandler(tornado.web.RequestHandler):
+	def get(self, control):
+		print control
+		os.system("osascript applescripts/" + control + ".scpt")
+		self.write("OK")
+				
+class IndexHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.set_header('Content-type', 'application/xhtml+xml')
+		return self.render('remote.xhtml')
+		
+application = tornado.web.Application([
+	(r"/", IndexHandler),
+	(r"/favicon\.ico", lambda x, y: None),
+	(r"/(.*)", RemoteHandler),
+])
 	
 def main():
-	port = 8888
 	pybonjour.DNSServiceRegister(
 						name = 'Apple Remote',
 						regtype = '_http._tcp',
 						port = port,
 						callBack = None,
 						txtRecord = pybonjour.TXTRecord({'path': '/'}),)
-					
-	try:
-		server = HTTPServer(('', port), RemoteHandler)
-		print 'started server...'
-		server.serve_forever()
-		
-	except KeyboardInterrupt:
-		print 'Received interrupt, shutting down'
-		server.socket.close()
+						
+	http_server = tornado.httpserver.HTTPServer(application)
+	http_server.listen(port)
+	tornado.ioloop.IOLoop.instance().start()
 	
 if __name__ == '__main__':
 	main()
